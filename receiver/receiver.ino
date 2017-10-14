df60ca24667c651f70756c7d06a07e958d334634
Alex Nussey <alex@nussey.com>
2017-10-14 03:42:08 -0400

    Initial serial communication with receiver

---
+++ receiver/receiver.ino
@@ -0,0 +1,16 @@
+void setup() {
+    // start serial port at 9600 bps:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+}
+
+void loop() {
+    // if we get a valid byte, read analog ins:
+    //if (Serial.available() > 0) {
+      // get incoming byte:
+      //inByte = Serial.read();
+    Serial.write("Hello, World!\n");
+    delay(1000);
+}
--- server/server.go
+++ server/server.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/tarm/serial"
+)
+
+const (
+	serialPort = "/dev/cu.usbmodem14221"
+	baudRate   = 9600
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	mon := newSerialMonitor(serialPort, baudRate)
+	for true {
+		fmt.Println("diddle")
+		fmt.Println(mon.readln())
+	}
+}
+
+type serialMonitor struct {
+	sm *serial.Port
+
+	buffer []byte
+	out    string
+	port   string
+}
+
+func newSerialMonitor(port string, baud int) serialMonitor {
+	var mon serialMonitor
+	mon.buffer = make([]byte, 1)
+	mon.port = port
+
+	c := &serial.Config{Name: port, Baud: baud}
+	s, _ := serial.OpenPort(c)
+
+	mon.sm = s
+
+	return mon
+}
+
+func (s *serialMonitor) readln() string {
+	s.out = ""
+	for true {
+		_, err := s.sm.Read(s.buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c := string(s.buffer[0])
+		if c == "\n" {
+			return s.out
+		}
+		s.out += c
+	}
+
+	return ""
 }
