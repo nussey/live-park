2dee858dd2f61f9df4eccb12b4ba44486aa714ad
Alex Nussey <alex@nussey.com>
2017-10-14 08:45:01 -0400

    really get started

--- server/server.go
+++ server/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"sync"
 
-	"github.com/tarm/serial"
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -12,48 +14,52 @@ const (
 	baudRate   = 9600
 )
 
+var printer chan string
+
+type ParkingSpot struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	sync.Mutex
+
+	// What format can I give you a geofence in?
+	name  string
+	spots []ParkingSpot
+}
+
+func newParkingLot(name string, spots int) *ParkingLot {
+	var lot ParkingLot
+	lot.name = name
+	lot.spots = make([]ParkingSpot, spots)
+
+	return &lot
+}
+
 func main() {
+	printer = make(chan string)
+
 	mon := newSerialMonitor(serialPort, baudRate)
+
+	lot := newParkingLot("Howey Lot", 10)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/foobar", lot.TestHandler).Methods("GET")
+	go func() { log.Fatal(http.ListenAndServe(":8080", router)) }()
+
+	go func() {
+		for true {
+			printer <- "diddle"
+			printer <- mon.readln()
+		}
+	}()
+
 	for true {
-		fmt.Println("diddle")
-		fmt.Println(mon.readln())
+		msg := <-printer
+		fmt.Println(msg)
 	}
 }
 
-type serialMonitor struct {
-	sm *serial.Port
-
-	buffer []byte
-	out    string
-	port   string
-}
-
-func newSerialMonitor(port string, baud int) serialMonitor {
-	var mon serialMonitor
-	mon.buffer = make([]byte, 1)
-	mon.port = port
-
-	c := &serial.Config{Name: port, Baud: baud}
-	s, _ := serial.OpenPort(c)
-
-	mon.sm = s
-
-	return mon
-}
-
-func (s *serialMonitor) readln() string {
-	s.out = ""
-	for true {
-		_, err := s.sm.Read(s.buffer)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c := string(s.buffer[0])
-		if c == "\n" {
-			return s.out
-		}
-		s.out += c
-	}
-
-	return ""
+func (pl *ParkingLot) TestHandler(w http.ResponseWriter, r *http.Request) {
+	printer <- "FOOBAR"
 }
