7fdbea49b6775e44eeda0d7e81e437c5a4d4d7f0
Alex Nussey <alex@nussey.com>
2017-10-14 14:32:07 -0400

    touch

---
+++ server/structs.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"sync"
+
+	"github.com/nussey/live-park/server/geo"
+)
+
+type ParkingSpot struct {
+	occupied bool
+	reserved bool
+
+	Location geo.Point
+	Name     string
+
+	HardwareId int64
+	battery    int
+}
+
+type ParkingLot struct {
+	sync.Mutex
+
+	name string
+	// dollars per hour
+	price float32
+
+	spots map[int64]*ParkingSpot
+
+	entrace  geo.Point
+	geofence []geo.Point
+}
+
+func spotsToMap(spots []*ParkingSpot) map[int64]*ParkingSpot {
+	m := make(map[int64]*ParkingSpot)
+	for _, spot := range spots {
+		m[spot.HardwareId] = spot
+	}
+
+	return m
+}
+
+func mapToSpots(m map[int64]*ParkingSpot) []*ParkingSpot {
+	spots := make([]*ParkingSpot, len(m))
+
+	i := 0
+	for _, spot := range m {
+		spots[i] = spot
+		i++
+	}
+
+	return spots
+}
+
+func (p *ParkingLot) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Name           string
+		Price          float32
+		AvailableSpots int
+		TotalSpots     int
+
+		Entrace  geo.Point
+		GeoFence []geo.Point
+		Spots    []*ParkingSpot
+	}{
+		Name:           p.name,
+		Price:          p.price,
+		AvailableSpots: 0,
+		TotalSpots:     1,
+
+		Entrace:  p.entrace,
+		GeoFence: p.geofence,
+		Spots:    mapToSpots(p.spots),
+	})
+}
+
+func (p *ParkingLot) UnmarshalJSON(b []byte) error {
+	type unmar struct {
+		Name     string
+		Price    float32
+		Entrance geo.Point
+		GeoFence []geo.Point
+		Spots    []*ParkingSpot
+	}
+
+	var d unmar
+
+	err := json.Unmarshal(b, &d)
+	if err != nil {
+		return err
+	}
+
+	p.name = d.Name
+	p.price = d.Price
+	p.entrace = d.Entrance
+	p.geofence = d.GeoFence
+	p.spots = spotsToMap(d.Spots)
+
+	return nil
+}
