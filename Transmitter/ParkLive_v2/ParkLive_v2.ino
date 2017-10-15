668d0cf98ecd4cdb4b71af7290462bc1e1931b87
Ryan Babaie <rbabaie@gatech.edu>
2017-10-15 02:52:41 -0400

    Added JSON output to receiver, transmitter

--- Transmitter/ParkLive_v2/ParkLive_v2.ino
+++ Transmitter/ParkLive_v2/ParkLive_v2.ino
@@ -1,75 +1,90 @@
 #include <RH_ASK.h>
 #include <SPI.h>
+#define N_POINTS 100
+#define HEARTBEAT_PERIOD  30000
 
 RH_ASK driver;
-#define n_points 50
 int sensorpin = 0;
 int val;
-float oldvoltage[n_points];
+float oldvoltage[N_POINTS];
 bool state = 1;
 bool previous_state = 1;
 bool is_changed = 0;
 unsigned long timer;
+float batteryPercentage = 100.0;
 
 
 void setup() {
   Serial.begin(9600);
-  //Serial.println("FIRST I PARK MY CAR.........");
+  Serial.println("FIRST I PARK MY CAR.........");
   if (!driver.init()) {
     Serial.println("init failed");
   }
   
-  for(int i = 0; i < n_points; i++) {
+  for(int i = 0; i < N_POINTS; i++) {
     val = analogRead(sensorpin);
     oldvoltage[i] = 0;
   }
 }
 
+struct _packet {
+  uint32_t identifier;
+  uint8_t occupied;
+  uint8_t battery_percentage;
+} packetdata;
+
+float voltage;
+long lastUpdate = 0;
 void loop() {
   timer = millis();
   
   if (timer%10 == 0) {
     val = analogRead(sensorpin);
+    batteryPercentage -= .01 * .01;
+    if(batteryPercentage < 0)
+      batteryPercentage = 0;
 
-    float voltage = 5.0 / 1024 * val;
+    voltage = 5.0 / 1024 * val;
     uint8_t j;
-    for(j = 1; j < n_points; j++) {
+    for(j = 1; j < N_POINTS; j++) {
       oldvoltage[j-1] = oldvoltage[j];
     }
-    oldvoltage[n_points-1] = voltage;
-    for(j = 0; j < n_points; j++) {
+    oldvoltage[N_POINTS-1] = voltage;
+    for(j = 0; j < N_POINTS; j++) {
       voltage += oldvoltage[j];
     }
 
-    voltage /= (n_points + 1);
+    voltage /= (N_POINTS + 1);
+    if(state) {
+      if(voltage <= 1.0 && oldvoltage[N_POINTS/2] > voltage) {
+        state = 0;
+      }
+    } else {
+      if(voltage >= 1.0 && oldvoltage[N_POINTS/2] < voltage) {
+        state = 1;
+      }
+    }
     
-    state = (voltage >= 0.31) && (voltage <= 1.0);
-    struct _packet {
-      uint32_t identifier;
-      uint8_t occupied;
-      uint8_t battery_percentage;
-    } packetdata;
-  
     packetdata.identifier = 0x69696969;
     packetdata.occupied = state;
-    packetdata.battery_percentage = 0x69;
+    packetdata.battery_percentage = batteryPercentage / 100.0 * 255;
   
     is_changed = !(previous_state == state);
   
-    //Serial.println(voltage);
+    Serial.println(voltage);
     
     if (is_changed) {
         driver.send((uint8_t*)&packetdata,sizeof(struct _packet));
-        Serial.println("SENT");
+        //Serial.println("SENT");
         if (state) {
-          //Serial.println("PARKED");
+          Serial.println("PARKED");
         } else {
-          //Serial.println("EMPTY");
+          Serial.println("EMPTY");
         }
-        //Serial.println(voltage);
+        Serial.println(voltage);
         previous_state = state;
     } 
-    if (timer%30000 == 0) {
+    if (timer % HEARTBEAT_PERIOD == 0) {
         driver.send((uint8_t*)&packetdata,sizeof(struct _packet));
         Serial.println("SENT EVERY 30");
         delay(1);
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,16 +1,114 @@
+#include "./libs/VirtualWire/VirtualWire.cpp"
+#include "./libs/ArduinoJson/ArduinoJson.h"
+
+#define PACKET_LENGTH     11      // 11 bytes after preamble
+
+#define STATE_IDLE        0
+#define STATE_PREAMBLE_1  1
+#define STATE_PREAMBLE_2  2
+#define STATE_PREAMBLE_3  3
+#define STATE_RECV        4
+
+
+uint8_t buf[16];
+uint8_t bufLen = 16;
+
+// State machine variables
+uint8_t state = STATE_IDLE;
+uint8_t bytesReceived = 0;
+
+struct _packet {
+  uint32_t identifier;
+  uint8_t occupied;
+  uint8_t batteryPercentage;
+} packetData;
+uint8_t* packetPtr = (uint8_t*)&packetData;
+
 void setup() {
     // start serial port at 9600 bps:
     Serial.begin(9600);
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB port only
     }
+    
+    vw_set_ptt_inverted(true);
+    vw_setup(2000);
+    vw_rx_start();
 }
 
+void reverse(uint8_t *src, uint8_t len) {
+  uint8_t tmp;
+  for(uint8_t j = 0; j < len; j++) {
+    tmp = src[j];
+    src[j] = src[len-j-1];
+    src[len-j-1] = tmp;
+  }
+}
+
+char stringBuf[100];
 void loop() {
-    // if we get a valid byte, read analog ins:
-    //if (Serial.available() > 0) {
-      // get incoming byte:
-      //inByte = Serial.read();
-    Serial.write("Hello, World!\n");
-    delay(1000);
+  uint8_t i;
+  if(vw_get_message(buf, &bufLen)) {
+    /*Serial.println("Got message: ");
+     for (i = 0; i < bufLen; i++) {
+      Serial.print(buf[i], HEX);
+      Serial.print(" ");
+     }
+     Serial.println();*/
+    packetData.identifier = *((uint32_t*)(buf+4));
+    packetData.occupied = buf[8];
+    packetData.batteryPercentage = buf[9];
+    
+    StaticJsonBuffer<101> jsonBuffer;
+    JsonObject& obj = jsonBuffer.createObject();
+    obj["identifier"] = packetData.identifier;
+    obj["batteryPercentage"] = packetData.batteryPercentage;
+    obj["occupied"] = packetData.occupied;
+
+    obj.printTo(Serial);
+    Serial.println();
+  }
+
+  /*
+  for(i = 0; i < bufLen; i++) {
+    switch(state) {
+      case STATE_IDLE:
+        if(buf[i] == 0x50) {
+          state = STATE_PREAMBLE_1;
+        }
+        break;
+      case STATE_PREAMBLE_1:
+        if(buf[i] == 0x41) {
+          state = STATE_PREAMBLE_2;
+        } else {
+          state = STATE_IDLE;
+        }
+        break;
+      case STATE_PREAMBLE_2:
+        if(buf[i] == 0x52) {
+          state = STATE_PREAMBLE_3;
+        } else {
+          state = STATE_IDLE;
+        }
+      case STATE_PREAMBLE_3:
+        if(buf[i] == 0x4B) {
+          state = STATE_RECV;
+        } else {
+          state = STATE_IDLE;
+        }
+        break;
+      case STATE_RECV:
+        if(bytesReceived < PACKET_LENGTH) {
+
+          // Got a full packet!
+          packetPtr[bytesReceived] = buf[i];
+          bytesReceived++;
+
+          // Check CRC here
+        } else {
+          state = STATE_IDLE;
+          bytesReceived = 0;
+        }
+    }
+    */
 }
